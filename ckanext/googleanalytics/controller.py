8810b89122e1c5758918515feaa423761a67bbac
Ankit Rana <arana@opengov.com>
2018-08-14 12:25:42 -0400

    Added if/else in read operations for Orgs to prevent read on org 'new'

--- ckanext/googleanalytics/controller.py
+++ ckanext/googleanalytics/controller.py
@@ -178,10 +178,20 @@ class GAOrganizationController(OrganizationController):
             }
             plugin.GoogleAnalyticsPlugin.analytics_queue.put(data_dict)
 
+    # def read(self, id, limit=20):
+    #     org = toolkit.get_action('organization_show')({},{'id':id})
+    #     org_title = org.get('title')
+    #     self._post_analytics(c.user,"Organization","View",org_title)
+    #     return OrganizationController.read(self, id, limit=20)
+
     def read(self, id, limit=20):
-        org = toolkit.get_action('organization_show')({},{'id':id})
-        org_title = org.get('title')
-        self._post_analytics(c.user,"Organization","View",org_title)
+        # We do not want to incorrectly perform read operation on organization id "new", where it results in a NotFound error 
+        if id!="new":
+            org = toolkit.get_action('organization_show')({},{'id':id})
+            org_title = org.get('title')
+            self._post_analytics(c.user,"Organization","View",org_title)
+        else:
+            return OrganizationController.new(self)
         return OrganizationController.read(self, id, limit=20)
 
 class GAPackageController(PackageController):
