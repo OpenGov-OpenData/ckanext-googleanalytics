f13b8d017be0716c71b2e68745d3137b328a3897
Jay Guo <jguo144@gmail.com>
2018-08-24 15:44:15 -0400

    Fix for package NotFound error

--- ckanext/googleanalytics/controller.py
+++ ckanext/googleanalytics/controller.py
@@ -179,11 +179,12 @@ class GAOrganizationController(OrganizationController):
             plugin.GoogleAnalyticsPlugin.analytics_queue.put(data_dict)
 
     def read(self, id, limit=20):
-        # We do not want to incorrectly perform read operation on organization id "new", where it results in a NotFound error 
+        # We do not want to perform read operation on organization id "new",
+        # where it results in a NotFound error
         if id!="new":
             org = toolkit.get_action('organization_show')({},{'id':id})
             org_title = org.get('title')
-            self._post_analytics(c.user,"Organization","View",org_title)
+            self._post_analytics(c.user,"Organization", "View", org_title)
         else:
             return OrganizationController.new(self)
         return OrganizationController.read(self, id, limit=20)
@@ -206,23 +207,32 @@ class GAPackageController(PackageController):
             }
             plugin.GoogleAnalyticsPlugin.analytics_queue.put(data_dict)
 
-    # This function is called everytime we access a dataset including the dataset "new" when creating a new datasets
+    # This function is called everytime we access a dataset including
+    # the dataset "new" when creating a new datasets
     def read(self, id):
-        # We do not want to incorrectly perform read operation on package id "new", where it results in the package not being found
+        # We do not want to perform read operation on package id "new",
+        # where it results in the package not being found
         if id!="new":
             org_id = self.get_package_org_id(id)
-            self._post_analytics(c.user,"Organization","View",org_id)
-        # If we simply return PackageController.read() or return w/o a PackaageController.new() operation, a blank page or error page will appear
+            if org_id:
+                self._post_analytics(c.user, "Organization", "View", org_id)
+        # If we simply return PackageController.read() or return w/o a
+        # PackageController.new() operation, a blank page or error page will appear
         else:
             return PackageController.new(self)
         return PackageController.read(self, id)
 
     def resource_read(self, id, resource_id):
         org_id = self.get_package_org_id(id)
-        self._post_analytics(c.user,"Organization","View",org_id)
+        if org_id:
+            self._post_analytics(c.user, "Organization", "View", org_id)
         return PackageController.resource_read(self, id, resource_id)
 
     def get_package_org_id(self, package_id):
-        package = toolkit.get_action('package_show')({},{'id':package_id})
-        org_id = package.get('organization').get('title')
+        org_id = ''
+        try:
+            package = toolkit.get_action('package_show')({},{'id':package_id})
+            org_id = package.get('organization').get('title')
+        except:
+            log.debug('Dataset not found: ' + package_id)
         return org_id
